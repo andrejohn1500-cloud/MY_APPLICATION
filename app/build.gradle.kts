92ed137893985fd1d17fb0f3ae9633541a2986dd
Andre John <andrejohn1500@gmail.com>
2026-04-19 14:20:40 +0000

    Add Rive dependency and teacher animation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,6 +53,9 @@ android {
 }
 
 dependencies {
+    implementation("app.rive:rive-android:9.6.2")
+    implementation("androidx.startup:startup-runtime:1.1.1")
+
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
